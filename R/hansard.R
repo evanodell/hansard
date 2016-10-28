7a152c3d7e1efe3b649fe06683eafaa7a25409ad
EvanOdell <evanodell91@gmail.com>
2016-10-28 08:37:03 +0000

    Basic framework of description file created

--- R/hansard.R
+++ R/hansard.R
@@ -543,7 +543,7 @@ commons_terms <- function(all=TRUE) {
 
 ### 23 PETITIONS - STILL NEED TO SORT THIS OUT
 
-written_questions <- function(all=TRUE) {
+petitions <- function(all=TRUE) {
 
   baseurl_writ <- "http://lda.data.parliament.uk/commonswrittenquestions.json"
 
@@ -561,40 +561,3 @@ written_questions <- function(all=TRUE) {
 }
 
 
-### 24
-
-written_questions <- function(all=TRUE) {
-
-  baseurl_writ <- "http://lda.data.parliament.uk/commonswrittenquestions.json"
-
-  writ <- fromJSON("http://lda.data.parliament.uk/commonswrittenquestions.json")
-
-  writJpage <- round(writ$result$totalResults/10+1,digits = 0)
-
-  pages <- list()
-
-  for(i in 0:writJpage){
-    mydata <- fromJSON(paste0(baseurl_writ, "?_page=", i), flatten=TRUE)
-    message("Retrieving page ", i)
-    pages[[i+1]] <- mydata$result$items
-  }
-}
-
-### 25
-
-written_questions <- function(all=TRUE) {
-
-  baseurl_writ <- "http://lda.data.parliament.uk/commonswrittenquestions.json"
-
-  writ <- fromJSON("http://lda.data.parliament.uk/commonswrittenquestions.json")
-
-  writJpage <- round(writ$result$totalResults/10+1,digits = 0)
-
-  pages <- list()
-
-  for(i in 0:writJpage){
-    mydata <- fromJSON(paste0(baseurl_writ, "?_page=", i), flatten=TRUE)
-    message("Retrieving page ", i)
-    pages[[i+1]] <- mydata$result$items
-  }
-}
