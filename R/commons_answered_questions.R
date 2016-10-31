7c5cde87cb121ad3e6283411698476963e4b9257
EvanOdell <evanodell91@gmail.com>
2016-10-31 11:08:19 +0000

    Mild updates

--- R/commons_answered_questions.R
+++ R/commons_answered_questions.R
@@ -10,19 +10,85 @@
 #' commons_answered_questions()
 
 
-commons_answered_questions <- function(all = TRUE) {
+commons_answered_questions <- function(all = TRUE, date=NA,
+                                       uin=NA, department=NA,
+                                       answeredby=NA) {
 
-  baseurl_comAnswered <- "http://lda.data.parliament.uk/commonsansweredquestions.json"
+  if(all==TRUE){
+      baseurl_comAnswered <- "http://lda.data.parliament.uk/commonsansweredquestions.json"
 
-  comAnswered <- jsonlite::fromJSON("http://lda.data.parliament.uk/commonsansweredquestions.json")
+      comAnswered <- jsonlite::fromJSON("http://lda.data.parliament.uk/commonsansweredquestions.json")
 
-  comAnsweredJpage <- round(comAnswered$result$totalResults/10 + 1, digits = 0)
+      comAnsweredJpage <- round(comAnswered$result$totalResults/10 + 1, digits = 0)
 
-  pages <- list()
+      pages <- list()
 
-  for (i in 0:comAnsweredJpage) {
-    mydata <- jsonlite::fromJSON(paste0(baseurl_comAnswered, "?_page=", i), flatten = TRUE)
-    message("Retrieving page ", i, " of ", comAnsweredJpage)
-    pages[[i + 1]] <- mydata$result$items
+      for (i in 0:comAnsweredJpage) {
+        mydata <- jsonlite::fromJSON(paste0(baseurl_comAnswered, "?_page=", i), flatten = TRUE)
+        message("Retrieving page ", i, " of ", comAnsweredJpage)
+        pages[[i + 1]] <- mydata$result$items
+    }
+
+  }else if (is.na(date)==FALSE){
+      baseurl_comAnswered <- "http://lda.data.parliament.uk/commonsansweredquestions.json"
+
+      comAnswered <- jsonlite::fromJSON(paste0(baseurl_comAnswered, "?date=", date))
+
+      comAnsweredJpage <- round(comAnswered$result$totalResults/10 + 1, digits = 0)
+
+      pages <- list()
+
+      for (i in 0:comAnsweredJpage) {
+        mydata <- jsonlite::fromJSON(paste0(baseurl_comAnswered, "?page=", i), flatten = TRUE)
+        message("Retrieving page ", i, " of ", comAnsweredJpage)
+        pages[[i + 1]] <- mydata$result$items
+    }
+
+  }else if (is.na(uin)==FALSE){
+    #   commonsansweredquestions?uin={uin}
+      baseurl_comAnswered <- "http://lda.data.parliament.uk/commonsansweredquestions.json"
+
+      comAnswered <- jsonlite::fromJSON(paste0(baseurl_comAnswered, "?uin=", uin))
+
+      comAnsweredJpage <- round(comAnswered$result$totalResults/10 + 1, digits = 0)
+
+      pages <- list()
+
+      for (i in 0:comAnsweredJpage) {
+        mydata <- jsonlite::fromJSON(paste0(baseurl_comAnswered, "?page=", i), flatten = TRUE)
+        message("Retrieving page ", i, " of ", comAnsweredJpage)
+        pages[[i + 1]] <- mydata$result$items
+    }
+
+  } else if(is.na(department)==FALSE){
+    #   commonsansweredquestions/answeringdepartment?q={query}
+      baseurl_comAnswered <- "http://lda.data.parliament.uk/commonsansweredquestions.json"
+
+      comAnswered <- jsonlite::fromJSON(paste0(baseurl_comAnswered, "?_page=0", "&AnsweringBody.=", department))
+
+      comAnsweredJpage <- round(comAnswered$result$totalResults/10 + 1, digits = 0)
+
+      pages <- list()
+
+      for (i in 0:comAnsweredJpage) {
+        mydata <- jsonlite::fromJSON(paste0(baseurl_comAnswered, "?_page=", i, "&AnsweringBody.=", department), flatten = TRUE)
+        message("Retrieving page ", i, " of ", comAnsweredJpage)
+        pages[[i + 1]] <- mydata$result$items
+    }
+  } else if(is.na(answeredby)==FALSE) {
+    #  commonsansweredquestions/answeredby/{mnisId}
+      baseurl_comAnswered <- "http://lda.data.parliament.uk/commonsansweredquestions.json"
+
+      comAnswered <- jsonlite::fromJSON(paste0(baseurl_comAnswered, "?date=", date))
+
+      comAnsweredJpage <- round(comAnswered$result$totalResults/10 + 1, digits = 0)
+
+      pages <- list()
+
+      for (i in 0:comAnsweredJpage) {
+        mydata <- jsonlite::fromJSON(paste0(baseurl_comAnswered, "?page=", i), flatten = TRUE)
+        message("Retrieving page ", i, " of ", comAnsweredJpage)
+        pages[[i + 1]] <- mydata$result$items
+    }
   }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# hansard
+# hansard: Accessing Westminster Parliament Data
 
-An R package to automatically fetch data from the UK parliament data API. Ironically, Hansard data is not yet accessible through the API. To explore all available data see http://explore.data.parliament.uk/. Help on the API is here: http://api.data.parliament.uk/help.
+An R package to automatically fetch data from the UK parliament data API. Ironically, Hansard data is not yet accessible through the API. To explore all available data see http://explore.data.parliament.uk/. Help on the API is here: http://api.data.parliament.uk/help. 
+
+The functions are works in progress. They can download all available data in each category, but I have not yet added functionality to accept queries.
