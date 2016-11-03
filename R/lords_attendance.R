e367886e070e0b2eabc1f39c7e96530987370efa
EvanOdell <evanodell91@gmail.com>
2016-11-03 13:14:13 +0000

    Improved Lords Attendance

--- R/lords_attendance.R
+++ R/lords_attendance.R
@@ -4,15 +4,21 @@
 #' House of Lords Attendance
 #'
 #' This imports data on House of Lords attendance
-#' @param all Imports all available House of Lords attendance records Defaults to TRUE.
+#' @param type Accepts arguments "all" and "date"
+#' @param all Imports all available House of Lords attendance records
+#' @param date all available House of Lords attendance records on a given date
 #' @keywords House of Lords Attendance
 #' @export
 #' @examples
 #' lords_attendance()
 
-lords_attendance <- function(all = TRUE) {
+lords_attendance <- function(type =c("all","date")) {
 
-    baseurl_lordsAttend <- " http://lda.data.parliament.uk/lordsattendances.json?_pageSize=500"
+  match.arg(type)
+
+  if(type=="all") {
+
+    baseurl_lordsAttend <- "http://lda.data.parliament.uk/lordsattendances.json?_pageSize=500"
 
     lordsAttend <- jsonlite::fromJSON(" http://lda.data.parliament.uk/lordsattendances.json?_pageSize=500")
 
@@ -25,6 +31,41 @@ lords_attendance <- function(all = TRUE) {
         message("Retrieving page ", i+1, " of ", lordsAttendJpage+1)
         pages[[i + 1]] <- mydata$result$items
     }
+  } else if(type=="date") {
+
+    attend.date <- readline("Enter date (yyyy-mm-dd): ")
+
+    baseurl_lordsAttend <- "http://lda.data.parliament.uk/lordsattendances/date/"
+
+    lordsAttend <- jsonlite::fromJSON(paste0(baseurl_lordsAttend,attend.date,".json?_pageSize=500"))
+
+    pages <- list()
+
+    for (i in 0:0) {
+      mydata <- jsonlite::fromJSON(paste0(baseurl_lordsAttend, "&_page=", i), flatten = TRUE)
+      message("Retrieving page ", i+1, " of ", 1)
+      pages[[i + 1]] <- mydata$result$items
+    }
+#  } else if(type=="ID") {
+
+# Lords.ID <- readline("Enter lords ID number: ")
+
+#    baseurl_lordsAttend <- "http://lda.data.parliament.uk/lordsattendances.json?mnisId="
+
+#    lordsAttend <- jsonlite::fromJSON(paste0(baseurl_lordsAttend,lords.ID,"&_pageSize=500"))
+
+#    lordsAttendJpage <- round(lordsAttend$result$totalResults/lordsAttend$result$itemsPerPage, digits = 0)
+
+#    pages <- list()
+
+#    for (i in 0:lordsAttendJpage) {
+#      mydata <- jsonlite::fromJSON(paste0(baseurl_lordsAttend, "&_page=", i), flatten = TRUE)
+#      message("Retrieving page ", i+1, " of ", lordsAttendJpage+1)
+#      pages[[i + 1]] <- mydata$result$items
+#    }
+  }
+
+
 
   df<- jsonlite::rbind.pages(pages[sapply(pages, length)>0]) #The data frame that is returned
 
