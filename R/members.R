b416bf043d71b74507f3ef0aeb6c56cfcefa57de
EvanOdell <evanodell91@gmail.com>
2016-11-15 22:39:56 +0000

    Fixes

--- R/members.R
+++ R/members.R
@@ -20,96 +20,96 @@
 #'}
 
 members <- function(house = c("all", "commons", "lords", "lordsInterests")) {
-
+  
   match.arg(house)
-
+  
   if (house == "all") {
-
+    
     baseurl_allMems <- "http://lda.data.parliament.uk/members.json?_pageSize=500"
-
+    
     allMems <- jsonlite::fromJSON("http://lda.data.parliament.uk/members.json?_pageSize=500")
-
+    
     allMemsJpage <- round(allMems$result$totalResults/allMems$result$itemsPerPage, digits = 0)
-
+    
     pages <- list()
-
+    
     for (i in 0:allMemsJpage) {
       mydata <- jsonlite::fromJSON(paste0(baseurl_allMems, "&_page=", i), flatten = TRUE)
       message("Retrieving page ", i + 1, " of ", allMemsJpage + 1)
       pages[[i + 1]] <- mydata$result$items
     }
-
+    
     df <- jsonlite::rbind.pages(pages[sapply(pages, length) > 0])  #The data frame that is returned
-
+    
   } else if (house == "commons") {
-
+    
     baseurl_allMems <- "http://lda.data.parliament.uk/commonsmembers.json?_pageSize=500"
-
+    
     allMems <- jsonlite::fromJSON("http://lda.data.parliament.uk/commonsmembers.json?_pageSize=500")
-
+    
     allMemsJpage <- round(allMems$result$totalResults/allMems$result$itemsPerPage, digits = 0)
-
+    
     pages <- list()
-
+    
     for (i in 0:allMemsJpage) {
       mydata <- jsonlite::fromJSON(paste0(baseurl_allMems, "&_page=", i), flatten = TRUE)
       message("Retrieving page ", i + 1, " of ", allMemsJpage + 1)
       pages[[i + 1]] <- mydata$result$items
     }
-
+    
     df <- jsonlite::rbind.pages(pages[sapply(pages, length) > 0])  #The data frame that is returned
-
+    
   } else if (house == "lords") {
-
+    
     baseurl_allMems <- "http://lda.data.parliament.uk/lordsmembers.json?_pageSize=500"
-
+    
     allMems <- jsonlite::fromJSON("http://lda.data.parliament.uk/lordsmembers.json?_pageSize=500")
-
+    
     allMemsJpage <- round(allMems$result$totalResults/allMems$result$itemsPerPage, digits = 0)
-
+    
     pages <- list()
-
+    
     for (i in 0:allMemsJpage) {
       mydata <- jsonlite::fromJSON(paste0(baseurl_allMems, "&_page=", i), flatten = TRUE)
       message("Retrieving page ", i + 1, " of ", allMemsJpage + 1)
       pages[[i + 1]] <- mydata$result$items
     }
-
+    
     df <- jsonlite::rbind.pages(pages[sapply(pages, length) > 0])  #The data frame that is returned
-
+    
   } else if (house == "lordsInterests") {
-
+    
     MP.ID <- readline("Enter the members ID number: ")
-
+    
     MP.ID <- URLencode(MP.ID)
-
+    
     baseurl_allMems <- "http://lda.data.parliament.uk/lordsregisteredinterests.json?_pageSize=500&member="
-
+    
     allMems <- jsonlite::fromJSON(paste0("http://lda.data.parliament.uk/lordsregisteredinterests.json?_pageSize=500&member=",
                                          MP.ID))
-
+    
     if (allMems$result$totalResults > allMems$result$itemsPerPage) {
-
+      
       allMemsJpage <- round(allMems$result$totalResults/allMems$result$itemsPerPage, digits = 0)
     } else {
       allMemsJpage <- 0
     }
-
+    
     pages <- list()
-
+    
     for (i in 0:allMemsJpage) {
       mydata <- jsonlite::fromJSON(paste0(baseurl_allMems, MP.ID, "&_page=", i), flatten = TRUE)
       message("Retrieving page ", i + 1, " of ", allMemsJpage + 1)
       pages[[i + 1]] <- mydata$result$items
     }
-
+    
     df <- jsonlite::rbind.pages(pages[sapply(pages, length) > 0])  #The data frame that is returned
   }
-
+  
   if (nrow(df) == 0) {
     message("The request did not return any data. Please check your search parameters.")
   } else {
     df
   }
-
+  
 }
