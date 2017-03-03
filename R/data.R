0493fd9d88dd283497baae1d1e52aa616ff6c9c1
EvanOdell <evanodell91@gmail.com>
2017-03-03 20:16:53 +0000

    added a bit more detail to the package overview

--- R/data.R
+++ R/data.R
@@ -1,22 +1,16 @@
-#' List of parliamentary research briefing topics
-#'
-#'
+#' A list of parliamentary research briefing topics. The list may be used to support calls to the \code{\link{research_briefings}} API.
 #' @format A list with twenty levels, one for each possible briefing topic.
 #' @source \url{http://explore.data.parliament.uk/}
 "research_topics_list"
 
 
-#' List of parliamentary research briefing topics and subtopics
-#'
-#'
+#' A list of parliamentary research briefing topics and subtopics.  The list may be used to support calls to the \code{\link{research_briefings}} API.
 #' @format A nested list with twenty levels, one for each possible briefing topic. Each level contains a list with all subtopics for the parent topic.
 #' @source \url{http://explore.data.parliament.uk/}
 "research_subtopics_list"
 
 
-#' List of parliamentary research briefing types
-#'
-#'
+#' A list of parliamentary research briefing types.  The list may be used to support calls to the \code{\link{research_briefings}} API.
 #' @format A list with six levels, one for each possible briefing type
 #' @source \url{http://explore.data.parliament.uk/}
 "research_types_list"
