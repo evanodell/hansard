7be7594def9673ba4fdc90613be90aa996321420
EvanOdell <evanodell91@gmail.com>
2017-03-05 15:00:21 +0000

    Started second vignette

--- R/papers_laid.R
+++ R/papers_laid.R
@@ -10,26 +10,25 @@
 #'
 
 papers_laid <- function(all) {
-    
-    if (paperType == "all") {
-        
-        baseurl_papers <- "http://lda.data.parliament.uk/paperslaid.json?_pageSize=500"
-        
+
+        baseurl <- "http://lda.data.parliament.uk/paperslaid.json?_pageSize=500"
+
         message("Connecting to API")
-        
-        papers <- jsonlite::fromJSON(baseurl_papers)
-        
+
+        papers <- jsonlite::fromJSON(baseurl)
+
         jpage <- round(papers$result$totalResults/papers$result$itemsPerPage, digits = 0)
-        
+
         pages <- list()
-        
-        for (i in 0:papersJpage) {
-            mydata <- jsonlite::fromJSON(paste0(baseurl_papers, "&_page=", i), flatten = TRUE)
-            message("Retrieving page ", i + 1, " of ", papersJpage + 1)
+
+        for (i in 0:jpage) {
+            mydata <- jsonlite::fromJSON(paste0(baseurl, "&_page=", i), flatten = TRUE)
+            message("Retrieving page ", i + 1, " of ", jpage + 1)
             pages[[i + 1]] <- mydata$result$items
         }
-    }
+
     df <- jsonlite::rbind.pages(pages[sapply(pages, length) > 0])  #The data frame that is returned
+
     if (nrow(df) == 0) {
         message("The request did not return any data. Please check your search parameters.")
     } else {
