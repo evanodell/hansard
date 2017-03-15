1c7c24d9b6288de4a8cc6deb25bc1222b3f51070
EvanOdell <evanodell91@gmail.com>
2017-03-15 14:01:22 +0000

    better test checks

--- tests/testthat/test_research_briefings.R
+++ tests/testthat/test_research_briefings.R
@@ -1,13 +1,34 @@
 
-context("research_briefings tests")
+context("research_briefings")
 
 
 test_that("research_briefings list calls work", {
-skip_on_cran()
+  skip_on_cran()
 
   rtl <- research_topics_list()
 
   expect_is(rtl, "list")
-  expect_length(rtl, 10)
+  expect_length(rtl, 20)
+
+  rsl <- research_subtopics_list()
+
+  expect_is(rsl, "list")
+  expect_length(rsl, 20)
+
+  rtyl <- research_types_list()
+
+  expect_is(rtl, "list")
+  expect_length(rtl, 6)
+
+})
+
+
+test_that("research_briefings return expected format", {
+  skip_on_cran()
+
+
+  rb_df <- research_briefings(subtopic = research_subtopics_list[[7]][10])
+
+  expect_is(rb_df, "data.frame")
 
 })
