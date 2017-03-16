d7280b120174a3e89c101e599bf3a2004b7db353
EvanOdell <evanodell91@gmail.com>
2017-03-16 13:40:26 +0000

    update of commons_terms

--- R/commons_terms.R
+++ R/commons_terms.R
@@ -4,7 +4,7 @@
 #'
 #' Imports the parliamentary thesaurus.
 #' @param search A string to search the parliamentary thesaurus for.
-#' @param class The class of definition to be returned Accepts one of "ID", "ORG", "SIT", "NAME", "LEG","CTP", "PBT", "TPG".  Defaults to NULL
+#' @param class The class of definition to be returned Accepts one of "ID", "ORG", "SIT", "NAME", "LEG","CTP", "PBT" and "TPG".  Defaults to NULL
 #' @param extra_args Additional parameters to pass to API. Defaults to NULL.
 #' @keywords parliamentary thesaurus
 #' @export
@@ -16,6 +16,15 @@
 
 commons_terms <- function(search=NULL, class=NULL, extra_args = NULL) {
 
+  if(is.null(class)==FALSE){
+
+    class_list <- list("ID", "ORG", "SIT", "NAME", "LEG", "CTP", "PBT", "TPG")
+
+    if(!(class %in% class_list)){
+      stop('Please check your class parameter. It must be one of "ID", "ORG", "SIT", "NAME", "LEG", "CTP", "PBT" or"TPG"', call. = FALSE)
+    }
+  }
+
   if(is.null(search)==FALSE){
     search <- utils::URLencode(search)
     search_query <- paste0("&_search=", search_query)
@@ -24,7 +33,7 @@ commons_terms <- function(search=NULL, class=NULL, extra_args = NULL) {
   }
 
   if(is.null(class)==FALSE){
-    class <-
+    class_query <- paste0("&class=",class)
   } else {
     class_query <- NULL
   }
@@ -33,14 +42,14 @@ commons_terms <- function(search=NULL, class=NULL, extra_args = NULL) {
 
     message("Connecting to API")
 
-    terms <- jsonlite::fromJSON(paste0(baseurl,search_query, extra_args), flatten = TRUE)
+    terms <- jsonlite::fromJSON(paste0(baseurl, search_query,class_query, extra_args), flatten = TRUE)
 
     jpage <- round(terms$result$totalResults/terms$result$itemsPerPage, digits = 0)
 
     pages <- list()
 
     for (i in 0:10) {
-        mydata <- jsonlite::fromJSON(paste0(baseurl, "&_page=", i, extra_args), flatten = TRUE)
+        mydata <- jsonlite::fromJSON(paste0(baseurl, search_query,class_query, "&_page=", i, extra_args), flatten = TRUE)
         message("Retrieving page ", i + 1, " of ", jpage + 1)
         pages[[i + 1]] <- mydata$result$items
     }
@@ -53,13 +62,3 @@ commons_terms <- function(search=NULL, class=NULL, extra_args = NULL) {
     }
 }
 
-
-x <- jsonlite::fromJSON("http://lda.data.parliament.uk/terms.json?_pageSize=500&_page=1")
-x2 <- jsonlite::fromJSON("http://lda.data.parliament.uk/terms.json?_pageSize=500&_page=2")
-x3 <- jsonlite::fromJSON("http://lda.data.parliament.uk/terms.json?_pageSize=500&_page=3")
-
-
-
-y3 <- x3$result$items$class
-
-z2 <- unique(y3)
