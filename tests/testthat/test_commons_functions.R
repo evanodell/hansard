7430dd6311d2213d4da56ab44d6775e65089d1e0
EvanOdell <evanodell91@gmail.com>
2017-05-02 08:42:50 +0100

    test notes

--- tests/testthat/test_commons_functions.R
+++ tests/testthat/test_commons_functions.R
@@ -4,33 +4,33 @@ context("commons functions")
 
 test_that("commons functions return expected format", {
     skip_on_cran()
-    
+
     xcaq <- commons_answered_questions(answering_department = "health", answered_by = "4019", start_date = "2017-02-01")
     expect_length(xcaq, 10)
     expect_type(xcaq, "list")
     expect_true(tibble::is_tibble(xcaq))
     Sys.sleep(45)
-    
+
     # Divisions
-    
+
     xcd <- commons_divisions(division_id = 694163, summary = FALSE)
     expect_length(xcd, 5)
     expect_type(xcd, "list")
     expect_true(tibble::is_tibble(xcd))
     Sys.sleep(45)
-    
+
     xcds <- commons_divisions(division_id = 694163, summary = TRUE)
     expect_length(xcds, 17)
     expect_type(xcds, "list")
     expect_true(tibble::is_tibble(xcds))
     Sys.sleep(45)
-    
+
     xcdall <- commons_divisions(start_date = "2017-02-01", end_date = "2017-03-01")
     expect_length(xcdall, 5)
     expect_type(xcdall, "list")
     expect_true(tibble::is_tibble(xcdall))
     Sys.sleep(45)
-    
+
     # Divisions by Date
     cdd <- commons_division_date("2017-02-24")
     expect_length(cdd, 5)
@@ -38,42 +38,42 @@ test_that("commons functions return expected format", {
     expect_true(tibble::is_tibble(cdd))
     Sys.sleep(45)
     expect_equal(nrow(cdd), 5)
-    
-    xcoqt <- commons_oral_question_times(session = "2016/17", question_id = "685697")
+
+    xcoqt <- commons_oral_question_times(session = "2016/17", question_id = "685697")### Function isn't returning tibble
     expect_length(xcoqt, 14)
     expect_type(xcoqt, "list")
     expect_true(tibble::is_tibble(xcoqt))
     Sys.sleep(45)
-    
+
     xcoqs <- commons_oral_question_times(session = "2015/16")
     expect_length(xcoqs, 14)
     expect_type(xcoqs, "list")
     expect_true(tibble::is_tibble(xcoqs))
     Sys.sleep(45)
     expect_equal(nrow(xcoqs), 313)
-    
+
     xcoqe <- commons_oral_questions(mp_id = 4019, answering_department = "education", tidy = FALSE)
     expect_length(xcoqe, 24)
     expect_type(xcoqe, "list")
     expect_true(tibble::is_tibble(xcoqe))
     Sys.sleep(45)
-    
+
     xcwq <- commons_written_questions(mp_id = 410, "cabinet office")
     expect_length(xcwq, 12)
     expect_type(xcwq, "list")
     expect_true(tibble::is_tibble(xcwq))
     Sys.sleep(45)
-    
+
     xcte <- commons_terms(search = "estate")
     expect_length(xcte, 20)
     expect_type(xcte, "list")
     expect_true(tibble::is_tibble(xcte))
     Sys.sleep(45)
-    
+
     xctec <- commons_terms(search = "estate", class = "ORG")
     expect_length(xctec, 19)
     expect_type(xctec, "list")
     expect_true(tibble::is_tibble(xctec))
     Sys.sleep(45)
-    
+
 })
