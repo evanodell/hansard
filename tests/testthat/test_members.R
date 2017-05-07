f514a6509536eae97597bfe8c110c3969c84ed12
EvanOdell <evanodell91@gmail.com>
2017-05-07 09:35:08 +0100

    members tests

--- tests/testthat/test_members.R
+++ tests/testthat/test_members.R
@@ -3,16 +3,13 @@ context("members")
 
 test_that("members functions return expected format", {
     skip_on_cran()
- skip_on_travis()
 
     # Members search
 
     mschris <- members_search("chris")
-
     expect_length(mschris, 12)
     expect_type(mschris, "list")
     expect_true(tibble::is_tibble(mschris))
-    
 
     # Lookup by member id
 
@@ -21,33 +18,33 @@ test_that("members functions return expected format", {
     expect_length(mabbot, 12)
     expect_type(mabbot, "list")
     expect_true(tibble::is_tibble(mabbot))
-    
+
 
     memempt <- members()
     expect_length(memempt, 12)
     expect_type(memempt, "list")
     expect_true(tibble::is_tibble(memempt))
-    
+
 
     camem <- commons_members()
 
     expect_length(camem, 12)
     expect_type(camem, "list")
     expect_true(tibble::is_tibble(camem))
-    
+
 
     lamem <- lords_members()
 
     expect_length(lamem, 9)
     expect_type(lamem, "list")
     expect_true(tibble::is_tibble(lamem))
-    
+
 
     lint <- lords_interests(530)
 
     expect_length(lint, 8)
     expect_type(lint, "list")
     expect_true(tibble::is_tibble(lint))
-    
+
 
 })
