3adf678b9627df6ed10c86bae05f14b296fb4174
EvanOdell <evanodell91@gmail.com>
2017-05-18 14:30:12 +0100

    test fixes

--- R/commons_divisions.R
+++ R/commons_divisions.R
@@ -20,43 +20,43 @@
 #' }
 
 commons_divisions <- function(division_id = NULL, summary = FALSE, start_date = "1900-01-01", end_date = Sys.Date(), extra_args = NULL, tidy = TRUE, tidy_style = "snake_case") {
-    
+
     dates <- paste0("&_properties=date&max-date=", as.Date(end_date), "&min-date=", as.Date(start_date))
-    
+
     if (is.null(division_id) == TRUE) {
-        
+
         baseurl <- "http://lda.data.parliament.uk/commonsdivisions"
-        
+
         message("Connecting to API")
-        
+
         divis <- jsonlite::fromJSON(paste0(baseurl, ".json?_pageSize=500", dates, extra_args))
-        
+
         jpage <- round(divis$result$totalResults/divis$result$itemsPerPage, digits = 0)
-        
+
         pages <- list()
-        
+
         for (i in 0:jpage) {
             mydata <- jsonlite::fromJSON(paste0(baseurl, ".json?_pageSize=500", dates, "&_page=", i, extra_args), flatten = TRUE)
             message("Retrieving page ", i + 1, " of ", jpage + 1)
             pages[[i + 1]] <- mydata$result$items
         }
-        
+
         df <- tibble::as_tibble(dplyr::bind_rows(pages))
-        
+
     } else if (is.null(division_id) == FALSE) {
-        
+
         division_id <- as.character(division_id)
-        
+
         baseurl <- "http://lda.data.parliament.uk/commonsdivisions/id/"
-        
+
         message("Connecting to API")
-        
+
         divis <- jsonlite::fromJSON(paste0(baseurl, division_id, ".json?", dates, extra_args), flatten = TRUE)
-        
+
         if (summary == TRUE) {
-            
+
             df <- divis$result$primaryTopic
-            
+
             df$AbstainCount <- df$AbstainCount$`_value`
             df$AyesCount <- df$AyesCount$`_value`
             df$Didnotvotecount <- df$Didnotvotecount$`_value`
@@ -67,39 +67,43 @@ commons_divisions <- function(division_id = NULL, summary = FALSE, start_date = 
             df$Margin <- df$Margin$`_value`
             df$Suspendedorexpelledvotescount <- df$Suspendedorexpelledvotescount$`_value`
             df$date <- df$date$`_value`
-            
+
             df <- tibble::as_tibble(df)
-            
+
         } else {
-            
+
             df <- tibble::as_tibble(divis$result$primaryTopic$vote)
-            
+
         }
-        
+
     }
-    
+
     if (nrow(df) == 0) {
         message("The request did not return any data. Please check your search parameters.")
     } else {
-        
+
         if (tidy == TRUE) {
-            
+
+          if(summary==TRUE){
+
             df$date._value <- as.Date(df$date._value)
-            
+
             df$date._datatype <- "Date"
-            
+
+          }
+
             df <- hansard_tidy(df, tidy_style)
-            
+
             df
-            
+
         } else {
-            
+
             df
-            
+
         }
-        
+
     }
-    
+
 }
 
 
@@ -116,54 +120,54 @@ commons_divisions <- function(division_id = NULL, summary = FALSE, start_date = 
 #' }
 #'
 commons_division_date <- function(date = NULL, extra_args = NULL, tidy = TRUE, tidy_style = "snake_case") {
-    
+
     if (is.null(date) == TRUE) {
         df <- commons_divisions()
     } else {
         date <- as.character(date)
         date <- paste0("&date=", date)
-        
+
         baseurl <- "http://lda.data.parliament.uk/commonsdivisions"
-        
+
         message("Connecting to API")
-        
+
         divis <- jsonlite::fromJSON(paste0(baseurl, ".json?_pageSize=500", date, extra_args))
-        
+
         jpage <- round(divis$result$totalResults/divis$result$itemsPerPage, digits = 0)
-        
+
         pages <- list()
-        
+
         for (i in 0:jpage) {
             mydata <- jsonlite::fromJSON(paste0(baseurl, ".json?_pageSize=500", date, "&_page=", i, extra_args), flatten = TRUE)
             message("Retrieving page ", i + 1, " of ", jpage + 1)
             pages[[i + 1]] <- mydata$result$items
         }
-        
+
         df <- dplyr::bind_rows(pages)
-        
+
         df <- tibble::as_tibble(df)
-        
+
         if (nrow(df) == 0) {
             message("The request did not return any data. Please check your search parameters.")
         } else {
-            
+
             if (tidy == TRUE) {
-                
+
                 df$date._value <- as.Date(df$date._value)
-                
+
                 df$date._datatype <- "Date"
-                
+
                 df <- hansard_tidy(df, tidy_style)
-                
+
                 df
-                
+
             } else {
-                
+
                 df
-                
+
             }
-            
+
         }
     }
-    
+
 }
