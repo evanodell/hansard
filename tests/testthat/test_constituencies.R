76a34d5b2a8352102c92634f4e9efe5cbcaee38f
EvanOdell <evanodell91@gmail.com>
2017-06-02 20:04:23 +0100

    tests

--- tests/testthat/test_constituencies.R
+++ tests/testthat/test_constituencies.R
@@ -2,12 +2,13 @@ library(hansard)
 context("constituencies")
 
 test_that("constituencies functions return expected format", {
-    
+
     skip_on_cran()
-    
-    ctx <- constituencies(current = TRUE)
+
+    ctxs <- constituencies(current = TRUE)
     expect_length(ctx, 9)
     expect_true(tibble::is_tibble(ctx))
     expect_equal(nrow(ctx), 650)
-    
+    expect_true(names(ctx)[5]=="ended_date_value")
+
 })
