f5855d944f83edc3a765a4a738be34fa0d700905
EvanOdell <evanodell91@gmail.com>
2017-06-02 20:20:55 +0100

    tests

--- tests/testthat/test_constituencies.R
+++ tests/testthat/test_constituencies.R
@@ -5,7 +5,7 @@ test_that("constituencies functions return expected format", {
 
     skip_on_cran()
 
-    ctxs <- constituencies(current = TRUE)
+    ctx <- constituencies(current = TRUE)
     expect_length(ctx, 9)
     expect_true(tibble::is_tibble(ctx))
     expect_equal(nrow(ctx), 650)
