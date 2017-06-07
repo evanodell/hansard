90e476198bf90aa0d00da58120da7e8b3ff4c267
EvanOdell <evanodell91@gmail.com>
2017-06-07 12:19:07 +0100

    update

--- R/research_briefings_lists.R
+++ R/research_briefings_lists.R
@@ -1,12 +1,10 @@
 
 
-
-#' research_topics_list
-#'
 #' A list of Parliamentary Research Briefings topics.
 #' @rdname research_briefings_lists
 #' @return A list with the different research topics available.
 #' @export
+#' @seealso \code{\link{research_briefings}}
 #' @keywords  Parliamentary Research Briefings
 #' @examples  \dontrun{
 #'
@@ -14,15 +12,15 @@
 #' }
 #'
 research_topics_list <- function() {
-    
+
     x <- jsonlite::fromJSON("http://lda.data.parliament.uk/researchbriefingtopics.json?", flatten = TRUE)
-    
+
     y <- x$result$items$prefLabel._value
-    
+
     research_topics_list <- as.list(y)
-    
+
     research_topics_list
-    
+
 }
 
 #' research_subtopics_list
@@ -40,29 +38,29 @@ research_topics_list <- function() {
 
 
 research_subtopics_list <- function() {
-    
+
     x <- jsonlite::fromJSON("http://lda.data.parliament.uk/researchbriefingtopics.json?", flatten = TRUE)
-    
+
     y <- x$result$items$prefLabel._value
-    
+
     research_topics_list <- as.list(y)
-    
+
     research_subtopics_list <- list()
-    
+
     for (i in research_topics_list) {
-        
+
         i <- utils::URLencode(i)
-        
+
         g <- jsonlite::fromJSON(paste0("http://lda.data.parliament.uk/researchbriefingsubtopics/", i, ".json?"), flatten = TRUE)
-        
+
         i <- utils::URLdecode(i)
-        
+
         research_subtopics_list[[i]] <- g$result$items$prefLabel._value
-        
+
     }
-    
+
     research_subtopics_list
-    
+
 }
 
 
@@ -81,11 +79,11 @@ research_subtopics_list <- function() {
 #' }
 
 research_types_list <- function() {
-    
+
     x <- jsonlite::fromJSON("http://lda.data.parliament.uk/researchbriefingtypes.json?")
-    
+
     research_types_list <- as.list(x$result$items$prefLabel$`_value`)
-    
+
     research_types_list
 }
 
