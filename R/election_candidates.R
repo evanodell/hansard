10777a22c3262591c1f20d15887c7a7ddd30a369
EvanOdell <evanodell91@gmail.com>
2017-06-18 19:50:45 +0100

    add identifier for aggregate

--- R/election_candidates.R
+++ R/election_candidates.R
@@ -76,7 +76,7 @@ election_candidates <- function(ID = NULL, constit_details = FALSE, extra_args =
     df2$about <- gsub("http://data.parliament.uk/resources/", "", df2$about)
     df2$about <- gsub("/.*", "", df2$about)
 
-    df2 <- aggregate(fullName._value ~ party._value + about, data = df2, c)
+    df2 <- stats::aggregate(fullName._value ~ party._value + about, data = df2, c)
     df2$fullName._value <- as.list(df2$fullName._value)
 
     dat[[i]] <- tidyr::spread(df2, party._value, fullName._value)
