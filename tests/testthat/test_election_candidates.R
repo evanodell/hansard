79eed468a01a74ab06f84a4ab0c4c944cec183d6
EvanOdell <evanodell91@gmail.com>
2017-08-10 16:24:55 +0100

    faster electoral test

--- tests/testthat/test_election_candidates.R
+++ tests/testthat/test_election_candidates.R
@@ -5,12 +5,12 @@ context("election_candidates")
 test_that("election_candidates return expected format", {
 
     skip_on_cran()
-    skip_on_travis()
+    #skip_on_travis()
 
-    xec <- hansard_election_candidates(ID=382037)
-    expect_length(xec, 127)
+    xec <- hansard_election_candidates(ID=650517)
+    expect_length(xec, 18)
     expect_type(xec, "list")
     expect_true(tibble::is_tibble(xec))
-    expect_true(nrow(xec)==650)
+    expect_true(nrow(xec)==1)
 
 })
