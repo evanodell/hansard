2462fc50b14b636f70ea23eb9fda2e872da66f6b
EvanOdell <evanodell91@gmail.com>
2017-08-14 11:48:43 +0100

    add gitignore

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 hansard_bom <- function(x) {
-    .Call(`_hansard_hansard_bom`, PACKAGE = 'hansard', x)
+    .Call('_hansard_hansard_bom', PACKAGE = 'hansard', x)
 }
 
